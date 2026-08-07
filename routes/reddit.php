2c2c0ac0d8ad6d7103b1a6e239476c8af6479662
Morgan McElroy <morganmcelroy@msn.com>
2026-08-07 12:04:02 -0700

    add feed controller to demo in web version

--- routes/reddit.php
+++ routes/reddit.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use App\Http\Controllers\FeedController;
 use App\Http\Controllers\RedditController;
 use App\Services\FeedSelector;
 use Illuminate\Support\Facades\Route;
@@ -9,3 +10,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/reddit/{subreddit?}', [RedditController::class, 'index'])->name('reddit.index');
 
 Route::get('/test/{subreddit}', fn (string $subreddit, FeedSelector $selector) => $selector->random('reddit', $subreddit));
+
+Route::get('/feed/{provider?}/{handle?}', [FeedController::class, 'index'])->name('feed.index');
